db5c41f3a0be6fec83d452f79eadd2dd524da1aa
marsh0517 <61434717+marsh0517@users.noreply.github.com>
2020-05-08 18:12:27 -0500

    code added for Transaction history to work

--- src/application/Model.java
+++ src/application/Model.java
@@ -15,16 +15,16 @@ public class Model {
 		public float calculate(long number1, long number2, String operator){
 			switch(operator){
 			case "+":
-				return number1 + number2;
+				return (float)number1 + (float)number2;
 			case "-":
-				return number1 - number2;
+				return (float)number1 - (float)number2;
 			case "/":
 				if(number2 == 0)
 					return 0;
 				else
-					return number1 / number2;
+					return (float)number1 / (float)number2;
 			case "*":
-				return number1 * number2;
+				return (float)number1 * (float)number2;
 			default:
 				break;
 			}
